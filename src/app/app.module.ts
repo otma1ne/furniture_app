47f6c46c8cbef7d09b8a6b85db7fc0c913f7be02
othm1n <othmanou01@gmail.com>
2023-04-25 11:57:31 +0100

    first commit add home page header hero section categorie trending

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SwiperModule } from 'swiper/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeroComponent } from './home/hero/hero.component';
+import { PrimaryBtnComponent } from './shared/primary-btn/primary-btn.component';
+import { CategoriesComponent } from './home/categories/categories.component';
+import { TrendingComponent } from './home/trending/trending.component';
+import { ProductCardComponent } from './shared/product-card/product-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    CheckoutComponent,
+    HomeComponent,
+    ShopComponent,
+    HeaderComponent,
+    FooterComponent,
+    HeroComponent,
+    PrimaryBtnComponent,
+    CategoriesComponent,
+    TrendingComponent,
+    ProductCardComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, SwiperModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/home/hero/hero.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import SwiperCore, { Autoplay, EffectFade } from 'swiper';
+
+SwiperCore.use([Autoplay, EffectFade]);
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css'],
+})
+export class HeroComponent {}
